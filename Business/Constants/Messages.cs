15ac7bf2f95ee8e0cf5caa7e83b937516e620063
Oguzhan Kurt <Oguzhan Kurt@DESKTOP-CS0SANG>
2021-02-21 21:42:36 +0300

    Add project files.

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string PruductAdded = "Ürün eklendi";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz";
+        public static string MaintananceTime = "Sistem Bakımda";
+        public static string PruductListed = "Ürünler Listelendi";
+    }
+}
